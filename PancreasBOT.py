8f84301d6ddda0ee1a7d53c4fd53465a97050823
Pancreas <108845209+Pancraes@users.noreply.github.com>
2022-08-06 03:44:36 -0400

    Update PancreasBOT.py

--- PancreasBOT.py
+++ PancreasBOT.py
@@ -1,8 +1,10 @@
 import discord
 import json
 import random
+import string
 
 #https://discordapp.com/oauth2/authorize?client_id=1004603988282458113&scope=bot&permissions=8
+#git add ., git commit -am "ok", git push heroku master
 
 client = discord.Client()
 with open('PancreasBOTdata.json') as d:
@@ -12,18 +14,18 @@ data["images"]
 @client.event
 async def on_message(message):
     id = client.get_guild(1004603688062566430)
-    if message.content.find("hello") != -1:
+    if message.content.lower().find("hello") != -1:
         await message.channel.send("hElLo :nerd:")
 
-    if message.content.find("amogus") != -1:
+    if message.content.lower().find("amogus") != -1:
         await message.channel.send(":face_with_raised_eyebrow:")
 
-    if message.content.find("panc") != -1:
+    if message.content.lower().find("panc") != -1:
         if message.content.find(" ") == -1:
             await message.channel.send(f"""is kinda hot uwu 
         - {message.author}""")
  
-    if message.content.find("I would like to have some among us pictures please") != -1:
+    if message.content.lower().find("I would like to have some among us pictures please") != -1:
             await message.channel.send(data["images"][random.randint(0, len(data["images"])-1)])
 
 client.run("//redacted bc its my bot token//")
